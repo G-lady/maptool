bc7405dfd28a9f0cce3b02a92355fe583231faf5
GladysD <gladys.davidini@gmail.com>
2022-02-10 12:28:17 +0100

    my commit

---
+++ maptool.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+from PIL import Image
+img = Image.open("LabsinNigeria.png")
+ 
+st.image(img, width=700)
+
+st.title('Welcome to Laboratories in Nigeria')
+
+st.markdown('### Use this App to Easily Access Laboratories!')
+
+lab_name = st.text_input("Enter name of Laboratory:\n")
+
+lab_location = st.selectbox("Select Location: ",['Abuja', 'Port-Harcourt', 'Lagos', 'Ibadan'])
+
+lab_registration = st.number_input("MLSCN Registration Number")
+
+inquiry_type = st.selectbox('Select your staff type: ',('Admin', 'Laboratory Professionals', 'Cleaners',))
+
+number_of_staff = st.text_input("Number of Staff in lab")
+
+accreditation_date = st.date_input('MLSCN Accreditation Date')
+
+equipment_list = st.text("Enter your Equipments in lab:\n")
+
+if(st.button('Submit')):
+    result = equipment_list.title()"labmap" 
